eb92339f2481ec8e0a0f2dca7202e2afadde01ae
VIEYRA Kolawolé <kolawole.vieyra@groupe-esigelec.org>
2026-09-22 10:23:10 +0200

    Correctionn robot...

--- main.c
+++ main.c
@@ -6,33 +6,45 @@
 
 volatile unsigned int capt_opto_g = 0;
 volatile unsigned int capt_opto_d = 0;
+volatile int diff_capt = 0 ;
 
-#pragma vector=TIMER1_A0_VECTOR
+
+#define correction_rg (0)
+#define correction_rd (0)
+
+#pragma vector=PORT2_VECTOR
 __interrupt void capture_opto(void)
 { 
-  if((P2IN & BIT0)== BIT0){
-    capt_opto_g++;
+  if((P2IFG & BIT0) == BIT0){
+      capt_opto_g++;
+      P2IES ^= (BIT0); 
+    P2IFG &= ~BIT0;
+  }
+  if((P2IFG & BIT3) == BIT3){
+      capt_opto_d++;
+      P2IES ^= (BIT3); 
+    P2IFG &= ~BIT3;
   }
 
-  if((P2IN & BIT3)== BIT3){
-    capt_opto_d++;
-  }
 }
 
 void init_moteur(){
 
   // opto gauche
-  P2DIR &= ~BIT0; // P2.5 en sortie
-  P2SEL |= BIT0; // selection fonction TA1.2
-  P2SEL2 &= ~BIT0; // selection fonction TA1.2
+  P2DIR &= ~BIT0; // P2.0 en entrée
+  P2SEL &= ~BIT0; // selection fonction TA1.2
+  P2SEL2 &= ~BIT0; // selection fonction TA1.
 
   // opto droit
-  P2DIR &= ~BIT3; // P2.5 en sortie
-  P2SEL |= BIT3; // selection fonction TA1.2
+  P2DIR &= ~BIT3; // P2.3 en entrée
+  P2SEL &= ~BIT3; // selection fonction TA1.2
   P2SEL2 &= ~BIT3; // selection fonction TA1.
+  
+  P2IE |= (BIT0 | BIT3); 
+  P2IES |= (BIT0 | BIT3); 
 
   //config sens gauche et droit 
-  P2DIR |= (BIT1 | BIT5); // P2.5 en sortie
+  P2DIR |= (BIT1 | BIT5); // P2.5 et P2.1 en sortie
   P2SEL &= ~(BIT1 | BIT5); // selection fonction TA1.2
   P2SEL2 &= ~(BIT1 | BIT5); // selection fonction TA1.2
   // moteur gauche
@@ -56,6 +68,9 @@ void init_moteur(){
 
   P2OUT |= (BIT5);// sens
   P2OUT &= ~(BIT1);// sens
+    P2IFG &= ~BIT0;
+    P2IFG &= ~BIT3;
+
 }
 
 void pilotage_moteur(int sens_g, int puissance_g, int sens_d, int puissance_d){
@@ -73,8 +88,9 @@ void pilotage_moteur(int sens_g, int puissance_g, int sens_d, int puissance_d){
   {
     P2OUT &= ~(BIT1);// sens
   }
-  TA1CCR2 = FREQ_MOT*((float)puissance_d/100.0); // determine le rapport cyclique du signal
-  TA1CCR1 = FREQ_MOT*((float)puissance_g/100.0); // determine le rapport cyclique du signal
+  
+  TA1CCR2 = (FREQ_MOT*(puissance_d - correction_rg))/100; // determine le rapport cyclique du signal
+  TA1CCR1 = (FREQ_MOT*(puissance_g - correction_rd))/100; // determine le rapport cyclique du signal
 }
 int main(void) {
   volatile unsigned int i;
@@ -82,7 +98,6 @@ int main(void) {
 
   BCSCTL1= CALBC1_1MHZ; //frequence d’horloge 1MHz
   DCOCTL= CALDCO_1MHZ; // "
-
 
   P2DIR &= ~(BIT0 | BIT3); // P1.1 en entree
   P2SEL |= (BIT0 | BIT3); // fonction entree capture sur P1.1 (TA0.CCI0A)
@@ -92,7 +107,7 @@ int main(void) {
   TA1CCTL0 |= CAP | CCIE; // mode capture + autorisation interruption
 
   init_moteur();
-  pilotage_moteur(1,25,0,25);
+  pilotage_moteur(1,60,0,60);
 
   __enable_interrupt();
   while (1);
